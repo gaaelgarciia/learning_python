29696b32420afda4423e12634335decefac314df
gaaelgarciia <gael.garciaa@udc.es>
2024-02-20 00:15:00 +0100

    First commit to test the repo

---
+++ First_steps/Basics.py
@@ -0,0 +1,35 @@
+#In this first page i will be learning about the basic syntax of python
+
+#Printing a string
+
+print("Hello World")
+
+#Printing a variable
+
+x = "Hello World"
+
+print(x)    
+
+#Printing a number
+
+print(5)
+
+#Printing a variable and a number
+
+y = 5
+
+print(y)
+
+#Printing a variable and a string
+
+z = "Hello World"
+
+print(z)
+
+#Printing a variable and a number
+
+a = 5
+
+print(a)
+
+#Printing a variable and a string
